5d353b91c6be74118150cb68f1c4131e72eeeaea
goofyd <sravankumarmv@outlook.com>
2021-04-16 15:17:45 +0530

    Initial Commit with the below inclusions - Added Maven Dependencies - Add Log4j2 as Logging Service

---
+++ src/main/java/base/Runner.java
@@ -0,0 +1,10 @@
+package base;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+public class Runner {
+
+    private static final Logger LOGGER = LogManager.getLogger(Runner.class.getName());
+
+}
